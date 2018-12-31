0e03103101ffadddf342ddb1533e36b6782db9d8
Sander Bollen <a.h.b.bollen@lumc.nl>
2018-12-31 15:01:16 +0100

    process bam file to array

--- mrd/bam_process.py
+++ mrd/bam_process.py
@@ -14,4 +14,72 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+bam_process.py
+~~~~~~~~~~~~~~
+
+Process bam file to numpy array for classifications.
 """
+from pathlib import Path
+from typing import Iterator, Tuple, Callable
+
+import numpy as np
+from pysam import AlignmentFile
+
+
+def chop_contig(size: int, chunksize: int) -> Iterator[Tuple[int, int]]:
+    """
+    For a contig of given size, generate regions maximally chunksize long
+    We use _0_ based indexing
+    """
+    pos = 0
+    while pos < size:
+        end = pos + chunksize
+        if end < size:
+            yield (pos, end)
+        else:
+            yield (pos, size)
+        pos = end
+
+
+def softclip_bases(reader: AlignmentFile, contig: str,
+                   region: Tuple[int, int]) -> int:
+    """Calculate amount of softclip bases for a region"""
+    it = reader.fetch(contig=contig, start=region[0], stop=region[1])
+    s = 0
+    for read in it:
+        if read.cigartuples is not None:
+            s += sum(amount for op, amount in read.cigartuples if op == 4)
+    return s
+
+
+def coverage(reader: AlignmentFile, contig: str, region: Tuple[int, int],
+             method: Callable = np.mean) -> int:
+    """Calculate average/median/etc coverage for a region"""
+    covs = reader.count_coverage(contig=contig, start=region[0],
+                                 stop=region[1])
+
+    return method(np.sum(covs))
+
+
+def process_bam(path: Path, chunksize: int = 100,
+                contig: str = "chrM") -> np.ndarray:
+    """Process bam file to an ndarray"""
+    reader = AlignmentFile(str(path))
+    try:
+        ctg_idx = reader.references.index(contig)
+    except ValueError:
+        raise ValueError("Contig {0} does not exist in BAM file".format(
+            contig
+        ))
+    contig_size = reader.lengths[ctg_idx]
+
+    arr = []
+    for region in chop_contig(contig_size, chunksize):
+        block = []
+        n_reads = reader.count(contig=contig, start=region[0], stop=region[1])
+        cov = coverage(reader, contig, region)
+        softclip = softclip_bases(reader, contig, region)
+        block += [n_reads, cov, softclip]
+        arr += block
+    return np.array(arr)
