55221620f4f47d1bf55e74d9b46d23ff66e7e926
Sander Bollen <a.h.b.bollen@lumc.nl>
2019-01-23 16:44:22 +0100

    add chop contig test

--- tests/test_bam_process.py
+++ tests/test_bam_process.py
@@ -15,3 +15,21 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import pytest
+
+from rna_cd.bam_process import chop_contig
+
+
+chop_contig_data = [
+    ([1000, 100], [(0, 100), (100, 200), (200, 300), (300, 400), (400, 500),
+                   (500, 600), (600, 700), (700, 800), (800, 900),
+                   (900, 1000)]),
+    ([1000, 101], [(0, 101), (101, 202), (202, 303), (303, 404), (404, 505),
+                   (505, 606), (606, 707), (707, 808), (808, 909),
+                   (909, 1000)])
+]
+
+
+@pytest.mark.parametrize("args, expected", chop_contig_data)
+def test_chop_contig(args, expected):
+    assert list(chop_contig(*args)) == expected
